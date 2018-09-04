beabcc65178caba539883b6eac8dd06b4cda270a
Anthonio Daleo <antoniodaleo@outlook.com>
2018-09-03 21:14:22 -0300

    SASS

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+    // INDEX
+
+    include_once('_cabecalho.php');
+    
+    echo '<p>Teste de fontawasom <spam class="fa fa-times"></spam> </p>';
+
+
+    include_once('_rodape.php');
+?>
